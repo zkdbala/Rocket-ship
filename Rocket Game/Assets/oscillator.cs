1ad37237593c7fdfcc6d5b4dd3116e858de18766
Anshita Bala <anshita.bala2020@vitstudent.ac.in>
2022-01-17 19:45:41 +0530

    Project Uploaded

---
+++ Rocket Game/Assets/oscillator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movement = new Vector3(10f,10f,10f);
+    [SerializeField] float period = 2f;
+    float movementfactor;
+    Vector3 startingpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingpos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period; //grows continually from 0
+        const float tau = Mathf.PI*2;
+        float sinewave = Mathf.Sin(cycles * tau);
+        movementfactor = sinewave / 2f + 0.5f ;
+        Vector3 offset = movement * movementfactor;
+        transform.position = offset + startingpos;
+        
+    }
+}
---
+++ Rocket Game/Assets/rocket_ship.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class rocket_ship : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float thrustforce = 100f;
+    [SerializeField] float mainthrust = 10f;
+    [SerializeField] AudioClip main,death,nextlevel;
+    [SerializeField] ParticleSystem mainP, deathP, nextlevelP;
+    int n ;
+    [SerializeField] float LevelloadDelay = 1f;
+
+    enum State  {alive,death,wait};
+    State state = State.alive;
+    Rigidbody rigidbody;
+    AudioSource audio;
+    
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+        //firstlevel();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == State.alive)
+        {
+            ProcessInput();
+        }
+       
+    }
+
+    void ProcessInput()
+    {
+        Thrust();
+        Rotate();
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(state != State.alive)
+        {
+            return;
+        }
+        switch (collision.gameObject.tag)
+        {
+            case "friendly":
+                break;
+            case "Finish":
+                newlevel();
+                break;
+            default:
+                startagain();
+                break;
+        }
+
+    }
+
+    private void startagain()
+    {
+        state = State.death;
+        audio.Stop();
+        print("dead");
+        Invoke("dead", LevelloadDelay);
+        audio.PlayOneShot(death);
+        deathP.Play();
+    }
+
+    private void newlevel()
+    {
+        state = State.wait;
+        audio.Stop();
+        print("Hit Finish");
+        Invoke("LoadNextscene", LevelloadDelay);
+        audio.PlayOneShot(nextlevel);
+        nextlevelP.Play();
+        
+    }
+
+    private void dead()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextscene()
+    {
+        
+        int currentsceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextsceneindex = currentsceneIndex+1;
+        if (nextsceneindex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextsceneindex = 0;
+        }
+        SceneManager.LoadScene(nextsceneindex);
+
+    
+       
+
+    }
+    
+
+    private void Rotate()
+    {
+        
+        float rotationspeed = thrustforce * Time.deltaTime;
+        rigidbody.freezeRotation = true;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationspeed);
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationspeed);
+
+        }
+        rigidbody.freezeRotation = false;
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * mainthrust);
+            if (!audio.isPlaying)
+            {
+                audio.PlayOneShot(main);
+                mainP.Play();//to play the audio
+            }
+
+
+        }
+        else
+        {
+            audio.Stop();
+            mainP.Stop();
+
+        }
+    }
+}
